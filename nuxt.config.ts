7ffe8a35cf7055be0cba526a0304ef7280487fd2
xcia4899 <xcia48996531@gmail.com>
2026-01-12 16:14:40 +0800

    Add GitHub Actions deploy workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
+  app: {
+    baseURL: "Horizon-V2", // 重要：最後要有 /
+  },
   devtools: { enabled: true },
-  css: ['@/assets/scss/main.scss'],
+  css: ["@/assets/scss/main.scss"],
 
   // B. SCSS 全域注入（每個 <style lang="scss"> 都可直接用）
   vite: {
@@ -12,10 +15,10 @@ export default defineNuxtConfig({
           additionalData: `
             @use "@/assets/scss/variables.scss" as *;
             @use "@/assets/scss/mixins.scss" as *;
-          `
-        }
-      }
-    }
+          `,
+        },
+      },
+    },
   },
   modules: [
     "@nuxt/fonts",
@@ -23,5 +26,5 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "@nuxt/eslint",
     "@pinia/nuxt",
-  ]
+  ],
 });
