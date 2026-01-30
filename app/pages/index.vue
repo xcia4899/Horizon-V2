6368c9b028d4be668091248925819901c872486b
xcia4899 <xcia48996531@gmail.com>
2026-01-30 17:16:18 +0800

    html結構

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,15 +8,44 @@ feature-content
       </div>
     </section>
     <section class="home-products">home-products</section>
-
+    <section class="home-categories">home-categories</section>
     <section class="home-showcase">
       <div class="container showcase-inner">
         <HomeShowcase />
       </div>
     </section>
-
-    <section class="home-categories">home-categories</section>
-    <section class="home-promo">home-promo</section>
+    <section class="home-promo">
+      <div class="promo">
+        <div class="promo-media">
+          <img src="/images/home/promo/promo-01.png" alt="" />
+        </div>
+        <div class="promo-content">
+          <div class="promo-content-inner">
+            <h3 class="promo-title">簡約優雅 輕鬆制敵</h3>
+            <p class="promo-text">
+              ROG Falchion RX Low Profile 是一款精巧設計的 65%
+              電競鍵盤，結合優雅風格與強大效能。搭載全新的 ROG RX
+              矮軸與雙層消音泡棉，為您帶來無與倫比的打字與遊戲體驗。
+            </p>
+            <button class="promo-btn btn">了解更多</button>
+          </div>
+        </div>
+      </div>
+      <div class="promo">
+        <div class="promo-media"></div>
+        <div class="promo-content">
+          <div class="promo-content-inner">
+            <h3 class="promo-title">簡約優雅 輕鬆制敵</h3>
+            <p class="promo-text">
+              ROG Falchion RX Low Profile 是一款精巧設計的 65%
+              電競鍵盤，結合優雅風格與強大效能。搭載全新的 ROG RX
+              矮軸與雙層消音泡棉，為您帶來無與倫比的打字與遊戲體驗。
+            </p>
+            <button class="prome-btn btn">了解更多</button>
+          </div>
+        </div>
+      </div>
+    </section>
     <section class="home-brands">home-brands</section>
   </div>
 </template>
@@ -31,7 +60,6 @@ feature-content
     padding-inline: 0px;
   }
 }
-
 //--home-feature---------------
 .home-feature {
   background-color: var(--bg-surface-strong);
@@ -55,6 +83,43 @@ feature-content
     width: 100%;
     padding-block: 76px 64px;
     gap: 48px;
+  }
+}
+//home-promo
+.home-promo {
+  display: flex;
+  flex-direction: column;
+  .promo {
+    display: flex;
+    align-items: center;
+    // justify-content: flex-start;
+    width: 100%;
+    border: 1px solid red;
+    .promo-media {
+      flex: 0 0 50%;
+      // width: 50%;
+      height: auto;
+      aspect-ratio: 1 / 1;
+      background-color: #797979;
+      img {
+        width: 100%;
+        
+      }
+    }
+    .promo-content {
+      flex: 0 0 50%;
+      display: flex;
+      .promo-content-inner {
+        max-width: 600px;
+        // padding: 100px;
+      }
+    }
+  }
+  .promo:nth-child(even) {
+    flex-direction: row-reverse;
+    .promo-content {
+      justify-content: flex-end;
+    }
   }
 }
 </style>
