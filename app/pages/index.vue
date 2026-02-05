b2aecd740ea49334b505d48409ee3644e23f73be
xcia4899 <xcia48996531@gmail.com>
2026-02-05 14:50:34 +0800

    home hero html建立 基本css建立

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,43 +2,46 @@ feature-content
 <template>
   <div>
     <section class="home-hero">
-      <div class="swiper mySwiper">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide first-row">
-            <img src="./images/bg/index-first-bg-01.jpg" alt="" />
-            <div class="content-area frow-col frow-col-01">
-              <h3>始終電力十足。隨時待命。</h3>
-              <p>認識 POWERPLAY 2。無限力量，化繁為簡。</p>
-              <button class="btn first-btn-style">立即購買</button>
+      <div class="hero-inner">
+        <Swiper
+          class="hero-swiper-wrapper"
+          :modules="[Autoplay, Pagination, Navigation]"
+          :slides-per-view="1"
+          :space-between="0"
+          :loop="true"
+          :autoplay="{ delay: 10000, disableOnInteraction: false }"
+          :pagination="{ el: '.swiper-pagination', clickable: true }"
+          :navigation="{
+            nextEl: '.hero-swiper-btn-next',
+            prevEl: '.hero-swiper-btn-prev',
+          }"
+        >
+          <SwiperSlide v-for="i in 2" :key="i" class="hero-slide">
+            <div class="slide-image">
+              <img
+                class="image"
+                src="/images/bg/index-first-bg-01.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slide-content">
+              <h3 class="slide-title">始終電力十足。隨時待命。{{ i }}</h3>
+              <p class="slide-text">認識 POWERPLAY 2。無限力量，化繁為簡。</p>
+              <button class="btn slide-btn">立即購買</button>
+            </div>
+          </SwiperSlide>
+
+          <div class="swiper-pagination"></div>
+          <div class="hero-btnControl">
+            <div class="swiper-btn hero-swiper-btn-prev">
+              <Icon name="ep:arrow-left" class="icon" />
+            </div>
+            <div class="swiper-btn hero-swiper-btn-next">
+              <Icon name="ep:arrow-right" class="icon" />
             </div>
           </div>
-        </div>
-
-        <div class="swiper-btn content-area swiper-button-next"></div>
-        <div class="swiper-btn content-area swiper-button-prev"></div>
-        <div class="swiper-pagination frow-pagination"></div>
+        </Swiper>
       </div>
-      <Swiper
-        :modules="[Autoplay, Pagination, Navigation]"
-        :slides-per-view="1"
-        :space-between="0"
-        :loop="true"
-        :autoplay="{ delay: 6000, disableOnInteraction: false }"
-        :pagination="{ el: '.swiper-pagination', clickable: true }"
-        :navigation="{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        }"
-      >
-        <SwiperSlide v-for="i in 5" :key="i">
-          <div class="slide">Slide {{ i }}</div>
-        </SwiperSlide>
-
-        <!-- 這些 element 依舊要存在，給 Swiper 綁定 -->
-        <div class="swiper-pagination"></div>
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
-      </Swiper>
     </section>
 
     <section class="home-feature">
@@ -81,80 +84,47 @@ import "swiper/css/navigation";
   }
 }
 .home-hero {
-  .swiper-slide {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  .hero-inner {
   }
-
-  .swiper-slide img {
-    display: block;
-    width: 100%;
-    height: 100vh;
-    // max-height: 960px;
-    object-fit: cover;
-    object-position: bottom;
-  }
-
-  .swiper-btn {
-    margin: 0 32px;
-  }
-
-  &:hover .swiper-btn {
-    opacity: 1 !important;
-  }
-
-  .swiper-pagination {
-    position: absolute;
-    text-align: left;
-    margin-left: 5%;
-    bottom: 80px !important;
-  }
-
-  .swiper-pagination-bullet {
-    border-radius: 1px;
-
-    width: 120px;
-    height: 6px;
-    margin: 0 4px !important;
-
-    &::after {
-      content: "";
+  .hero-swiper-wrapper {
+    position: relative;
+    .hero-slide {
+      // min-height: 400px;
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .slide-image {
+        width: 100%;
+        .image {
+          display: block;
+          width: 100%;
+          height: 100vh;
+          // max-height: 960px;
+          object-fit: cover;
+          object-position: bottom;
+        }
+      }
+      .slide-content {
+        position: absolute;
+      }
+    }
+    .hero-btnControl{
+      display: flex;
+      justify-content: space-between;
+      pointer-events: none;
       position: absolute;
-      top: 0;
-      left: 0;
-      width: 0%;
-      height: 100%;
-
-      transition: none;
-      transform-origin: left;
-    }
-
-    &.progress::after {
-      transition: width 6s linear;
-      width: 100%;
-    }
-  }
-
-  .swiper-pagination-bullet-active {
-    opacity: 1;
-  }
-}
-
-.first-row {
-  .frow-col {
-    position: absolute;
-    flex-grow: 0.6;
-
-    padding: 0px 32px;
-    bottom: 40%;
-    left: 10vw;
-
-    .first-btn-style {
-      margin-top: 32px;
+      left: 48px;
+      right: 48px;
+      top: 50%;
+      z-index: 10;
+      .swiper-btn{
+        pointer-events: auto;
+      }
     }
   }
 }
+
 //打造你的理想戰場---------------
 .home-feature {
   background-color: var(--bg-surface-strong);
